d1d90922b4fc3590725f518ed88292c6ce65d5b8
Nasma <nasma.mlamali@icloud.com>
2020-12-08 14:19:16 -0800

    result display fixes

--- scripts/game.js
+++ scripts/game.js
@@ -1,192 +1,195 @@
 //prompt("This is a test");
 
 $(document).ready(function () {
-const playerDice1    = document.getElementById("player-dice1");
-const playerDice2    = document.getElementById("player-dice2");
-const playerScore1     = document.getElementById("player-score1");
-const playerScore2     = document.getElementById("player-score2");
-const playerScore3     = document.getElementById("player-score3");
 
-const computerDice1    = document.getElementById("computer-dice1");
-const computerDice2     = document.getElementById("computer-dice2");
-const computerScore1     = document.getElementById("computer-score1");
-const computerScore2     = document.getElementById("computer-score2");
-const computerScore3     = document.getElementById("computer-score3");
+    // Player dices
+    const playerDice1  = document.getElementById("player-dice1");
+    const playerDice2  = document.getElementById("player-dice2");
+    const playerScore1 = document.getElementById("player-score1");
+    const playerScore2 = document.getElementById("player-score2");
+    const playerScore3 = document.getElementById("player-score3");
 
-const buttonRollDices     = document.getElementById("roll-dices");
-const buttonRestart     = document.getElementById("restart");
+    const playerScoreTotal = document.getElementById("player-score-total");
 
-const mainPicture1InHTML = document.getElementById("dice-image1");
-const mainPicture2InHTML = document.getElementById("dice-image2");
-const mainPictureComp1InHTML = document.getElementById("comp-dice-image1");
-const mainPictureComp2InHTML = document.getElementById("comp-dice-image2");
-const scores = document.getElementById("score-title");
-const roundsScore1=document.getElementById("round1");
-const roundsScore2=document.getElementById("round2");
-const roundsScore3=document.getElementById("round3");
+    // Computer dices
+    const computerDice1  = document.getElementById("computer-dice1");
+    const computerDice2  = document.getElementById("computer-dice2");
+    const computerScore1 = document.getElementById("computer-score1");
+    const computerScore2 = document.getElementById("computer-score2");
+    const computerScore3 = document.getElementById("computer-score3");
 
-//count the rounds
-let roundCounter =0;
+    const computerScoreTotal = document.getElementById("computer-score-total");
 
-let gameCompleted=false;
+    // Buttons
+    const buttonRollDices = document.getElementById("roll-dices");
+    const buttonRestart   = document.getElementById("restart");
 
-// Dices
+    //Dices images
+    const mainPicture1InHTML     = document.getElementById("dice-image1");
+    const mainPicture2InHTML     = document.getElementById("dice-image2");
+    const mainPictureComp1InHTML = document.getElementById("comp-dice-image1");
+    const mainPictureComp2InHTML = document.getElementById("comp-dice-image2");
 
-let firstRoundPlayer=0;
-let firstRoundComputer=0;
-
-let scoreComputer1=0;
-let scorePlayer2=0;
-let scoreComputer2=0;
-let finalScorePlayer=0;
-let finalScoreComputer=0;
-let currentRoundPlayer= 0;
-
-
-//the first image # in the group
-let currentImageNumber = 1;
+    //Scores outputs
+    const scores       = document.getElementById("score-title");
+    const roundsScore1 = document.getElementById("round1");
+    const roundsScore2 = document.getElementById("round2");
+    const roundsScore3 = document.getElementById("round3");
+    const scoresTotal  = document.getElementById("roundsTotal");
    
-//the last image # in the group
-const maxImageNumber = 6;
 
-// roll the dices 
-$( buttonRollDices).click(function () {
+    //count the rounds
+    let roundCounter = 0;
 
-    let playerD1= Math.floor((Math.random() * 6) + 1);
-    let playerD2= Math.floor((Math.random() * 6) + 1);
-    let computerD1= Math.floor((Math.random() * 6) + 1);
-    let computerD2= Math.floor((Math.random() * 6) + 1);
+    let gameCompleted = false;
 
-    //Game conditions:
-    // If any of the dices is equal to 1 the round is equal 0
-    // If the dices throws a pair the scor should be doubled
-    // for player 
-    if((playerD1 == 1) || (playerD2 ==1)){
-         currentRoundPlayer= 0;
-    }else if (playerD1 == playerD2 ) {
-     currentRoundPlayer=  (playerD1+playerD2) *2;
-    }else{
-        currentRoundPlayer= playerD1+playerD2 
-    }
+    // Dices
 
-    //Game conditions for computer 
-    if((computerD1 == 1) || (computerD2 ==1)){
-         currentRoundComputer= 0;
-   }else if(computerD1 == computerD2) {
-     currentRoundComputer= (computerD1+computerD2)*2;
-   }else{
-        currentRoundComputer= (computerD1+computerD2);
-    }
+    let firstRoundPlayer = 0;
+    let firstRoundComputer = 0;
+
+    let scoreComputer1 = 0;
+    let scorePlayer2 = 0;
+    let scoreComputer2 = 0;
+    let finalScorePlayer = 0;
+    let finalScoreComputer = 0;
+    let currentRoundPlayer = 0;
 
 
-    roundCounter++;
-    console.log(roundCounter);
+    //the first image # in the group
+    let currentImageNumber = 1;
+
+    //the last image # in the group
+    const maxImageNumber = 6;
+
+    // roll the dices 
+    $(buttonRollDices).click(function () {
+
+        let playerD1 = Math.floor((Math.random() * 6) + 1);
+        let playerD2 = Math.floor((Math.random() * 6) + 1);
+        let computerD1 = Math.floor((Math.random() * 6) + 1);
+        let computerD2 = Math.floor((Math.random() * 6) + 1);
+
+        //Game conditions:
+        // If any of the dices is equal to 1 the round is equal 0
+        // If the dices throws a pair the scor should be doubled
+        // for player 
+        if ((playerD1 == 1) || (playerD2 == 1)) {
+            currentRoundPlayer = 0;
+        } else if (playerD1 == playerD2) {
+            currentRoundPlayer = (playerD1 + playerD2) * 2;
+        } else {
+            currentRoundPlayer = playerD1 + playerD2
+        }
+
+        //Game conditions for computer 
+        if ((computerD1 == 1) || (computerD2 == 1)) {
+            currentRoundComputer = 0;
+        } else if (computerD1 == computerD2) {
+            currentRoundComputer = (computerD1 + computerD2) * 2;
+        } else {
+            currentRoundComputer = (computerD1 + computerD2);
+        }
 
 
-if(roundCounter == 1){
- 
-    $(scores).text("Scores")
-   
-    $(playerDice1).text(playerD1);
-    $(playerDice2).text(playerD2);
-
-    $(roundsScore1).text("First Round");
-    firstRoundPlayer=currentRoundPlayer;
-    $(playerScore1).text( firstRoundPlayer);
-    mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
-    mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
-    $(playerScore1).text( "Player: " +firstRoundPlayer);
+        roundCounter++;
+        console.log(roundCounter);
 
 
-    $(computerDice1).text(computerD1);
-    $(computerDice2).text(computerD2);
-    firstRoundComputer= currentRoundComputer;
-    mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
-    mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
-    $(computerScore1).text( "Computer: " + firstRoundComputer);
-}if(roundCounter == 2){
-    $(roundsScore2).text("Second Round");
-    $(playerDice1).text(playerD1);
-    $(playerDice2).text(playerD2);
-    mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
-    mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
-    scorePlayer2= currentRoundPlayer+firstRoundPlayer;
+        if (roundCounter == 1) {
 
-    $(playerScore2).text("Player: "+scorePlayer2);
+            $(scores).text("Scores");
+
+            $(roundsScore1).text("First Round");
+            $(playerDice1).text(playerD1);
+            $(playerDice2).text(playerD2);
+           
+            mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
+            mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
+            firstRoundPlayer = currentRoundPlayer;
+       
+            $(playerScore1).text("Player: " + firstRoundPlayer);
 
 
-    $(computerDice1).text(computerD1);
-    $(computerDice2).text(computerD2);
-    scoreComputer2= firstRoundComputer+currentRoundComputer;
-    mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
-    mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
-    $(computerScore2).text("Computer: "+scoreComputer2);
+            $(computerDice1).text(computerD1);
+            $(computerDice2).text(computerD2);
+            mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
+            mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
+            firstRoundComputer = currentRoundComputer;
+            $(computerScore1).text("Computer: " + firstRoundComputer);
 
-    
-}else if(roundCounter == 3){
-    $(roundsScore3).text("Third Round");
-popUp();
-    gameCompleted=true;
+        } if (roundCounter == 2) {
 
-    $(buttonRollDices).hide();
-    $(playerDice1).text(playerD1);
-    $(playerDice2).text(playerD2);
-    mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
-    mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
-    finalScorePlayer= scorePlayer2+currentRoundPlayer;
-    $(playerScore3).text("Player: "+ finalScorePlayer);
+            $(roundsScore2).text("Second Round");
+            $(playerDice1).text(playerD1);
+            $(playerDice2).text(playerD2);
+            mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
+            mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
+            scorePlayer2 = currentRoundPlayer + firstRoundPlayer;
+            $(playerScore2).text("Player: " + currentRoundPlayer );
 
-
-    $(computerDice1).text(computerD1);
-    $(computerDice2).text(computerD2);
-    mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
-    mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
-    finalScoreComputer= scoreComputer2+currentRoundComputer;
-    $(computerScore3).text("Computer: "+finalScoreComputer);
-
-    if(finalScoreComputer > finalScorePlayer){
-        $("#result").text("Computer Wins");
-        return;
-    }else if(finalScoreComputer < finalScorePlayer){
-        $("#result").text("You Win");
-        return;
-    }else {
-        $("#result").text("No One Wins");
-        return;
-    }
-
-}
-
-    
+            $(computerDice1).text(computerD1);
+            $(computerDice2).text(computerD2);
+            mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
+            mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
+            scoreComputer2 = firstRoundComputer + currentRoundComputer;
+            $(computerScore2).text("Computer: " +  currentRoundComputer);
 
 
-// Reload the game when click on restart
-$(buttonRestart).click(function () {
-    location.reload();
-});
+        } else if (roundCounter == 3) {
+            $(roundsScore3).text("Third Round");
+            popUp();
+            gameCompleted = true;
+
+            $(buttonRollDices).hide();
+            $(playerDice1).text(playerD1);
+            $(playerDice2).text(playerD2);
+            mainPicture1InHTML.src = `images/player-dice-${playerD1}.svg`;
+            mainPicture2InHTML.src = `images/player-dice-${playerD2}.svg`;
+            finalScorePlayer = scorePlayer2 + currentRoundPlayer;
+            $(playerScore3).text("Player: " + currentRoundPlayer);
 
 
-//
-
-// const delay = 3000;
-// setTimeout(popUp, delay); 
-
-function popUp() {
-    if (roundCounter == 3) {
-        $("#pop-up-winner").animate({ opacity: '1',}, 2100);
-      
-    }
-}
+            $(computerDice1).text(computerD1);
+            $(computerDice2).text(computerD2);
+            mainPictureComp1InHTML.src = `images/computer-dice-${computerD1}.svg`;
+            mainPictureComp2InHTML.src = `images/computer-dice-${computerD2}.svg`;
+            finalScoreComputer = scoreComputer2 + currentRoundComputer;
+            $(computerScore3).text("Computer: " + currentRoundComputer);
+            $(scoresTotal).text("Total:");
+            $(playerScoreTotal).text("Player: "+finalScorePlayer);
+            $(computerScoreTotal).text("Computer: "+finalScoreComputer);
 
 
-//const closePopUpBtn = document.getElementById("btn-close");
+            if (finalScoreComputer > finalScorePlayer) {
+                $("#result").text("Computer Wins");
+                return;
+            } else if (finalScoreComputer < finalScorePlayer) {
+                $("#result").text("You Win");
+                return;
+            } else {
+                $("#result").text("No One Wins");
+                return;
+            }
 
-// closePopUpBtn.addEventListener("click", function () {
-//     $("#pop-up-winner).animate({ opacity: '0', }, 1800);
-// });
+        }
 
 
-});
+        // Reload the game when click on restart
+        $(buttonRestart).click(function () {
+            location.reload();
+        });
+
+
+        function popUp() {
+            if (roundCounter == 3) {
+                $("#pop-up-winner").animate({ opacity: '1', }, 3000);
+
+            }
+        }
+
+
+    });
 
 
 });
